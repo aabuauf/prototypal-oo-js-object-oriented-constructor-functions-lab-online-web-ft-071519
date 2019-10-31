b747063e22a4260ae12f891e7fe4d6e2572aba7d
Ahmed Abououf <aabuauf@yahoo.com>
2019-10-31 13:03:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function Scooter (year, color, model) {
     model,
   }
 }
+
+function Scooter (year, color, model) {
+    return {
+    year, // don't forget ES6 power-tools, this is the same as `name: name`
+    color,
+    model,
+  }
+}
